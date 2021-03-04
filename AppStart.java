d09b630a68cc23e6994028783e74fc8e7ce3db80
LuisFonseca <fonseca.luis.carlos50@gmail.com>
2021-03-04 10:46:16 +0000

    Classe AppStart

---
+++ AppStart.java
@@ -0,0 +1,12 @@
+public class AppStart
+{
+      public static void main(String [] args){
+        Size size=Size.MEDIUM;
+        System.out.println("Name            :" +size.name());
+        System.out.println("toString()      :" +size.toString());
+        System.out.println("Ordem           :" +size.ordinal());
+        System.out.println("Valor minimo    :" +size.getMinValue());
+        System.out.println("Valor maximo    :" +size.getMaxValue());
+        System.out.println("Codigo          :" +size.getCode());
+      }
+}
