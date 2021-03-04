4be58bd2f004ba9344d5b74d0ce7ee7c65609ee0
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2021-03-04 19:57:02 +0000

    Update AppStart.java

--- AppStart.java
+++ AppStart.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 public class AppStart
 {
       public static void main(String [] args){
-        Size size=Size.MEDIUM;
+        boolean validOption=false;
+        Size size = null;
+        Scanner sizeReader = new Scanner(System.in);
+        System.out.println("Bem Vindo ao meu programa :) \nEste programa tem como objetivo correr um enumerado de tamanhos\nInsira por favor uma das opções que se econtram abaixo \n");
+        while(validOption==false){
+            System.out.println("*************Tamanhos disponiveis***********");
+            System.out.println("*           Pequeno, Médio, Grande         *");
+            System.out.println("********************************************");
+            System.out.println("");
+            System.out.println("Tamanho ->");
+            String newSize = sizeReader.next();
+            if (newSize.equalsIgnoreCase("Pequeno")){
+                size = Size.SMALL;
+                validOption=true;
+            }else if(newSize.equalsIgnoreCase("Médio")){
+                size = Size.MEDIUM;
+                validOption=true;
+            }else if(newSize.equalsIgnoreCase("Grande")){
+                size = Size.LARGE;
+                validOption=true;
+            }else{
+                System.out.println("Erro! Tamanho invalido por favor insira novamente");
+                validOption=false;
+            }
+        }
+        
         System.out.println("Name            :" +size.name());
         System.out.println("toString()      :" +size.toString());
         System.out.println("Ordem           :" +size.ordinal());
