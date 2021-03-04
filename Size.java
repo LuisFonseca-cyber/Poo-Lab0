b28476d48cb0a7610bc1eb759781dfbb657b597b
LuisFonseca <fonseca.luis.carlos50@gmail.com>
2021-03-04 11:01:30 +0000

    comented Size Class

--- Size.java
+++ Size.java
@@ -4,11 +4,10 @@ public enum Size {
  MEDIUM(37, 44, "Médio",'M'),
  LARGE(45, 52, "Grande",'L');
  
- private final String description;
- private final int minValue;
- private final int maxValue;
- 
- private final char code;
+ private final String description; //representa a descriçãp
+ private final int minValue; //representa o valor minimo
+ private final int maxValue; //representa o valor maximo
+ private final char code; //representa o codigo
 
  private Size(int minValue, int maxValue, String description, char code){
      this.description=description;
@@ -16,19 +15,24 @@ public enum Size {
      this.maxValue=maxValue;
      this.code=code;
  }
+ //Este metedo devolve a descrição
  @Override
  public String toString() {
      return this.description;
  }
+ //Este metedo devolve a descrição
  public String getDescription() {
      return description;
  }
+ //Este metedo devolve o valor minimo
  public int getMinValue() {
      return minValue;
  }
+ //Este metedo devolve o valor maximo
  public int getMaxValue() {
      return maxValue;
  }
+ //Este metedo devolve o codigo
  public char getCode() {
      return code;
  }
